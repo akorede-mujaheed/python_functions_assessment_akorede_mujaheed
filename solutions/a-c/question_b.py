a4d95490d7d967bd99e86755ccc60fc792d3ad59
akorede_mujaheed <akorede_mujaheed>
2025-07-01 13:49:52 +0100

    solved positive numbers

---
+++ solutions/a-c/question_b.py
@@ -0,0 +1,42 @@
+def is_positive(number):
+    
+    intiger = number
+
+    if (intiger %2 ==0 ) and (intiger % 3 ==0 ):
+        return(intiger, "is true or fulse")
+    if intiger %2 ==0:
+        return intiger, "is true"  
+    if intiger % 3 == 0:
+        return intiger, "is fulse"   
+    
+print(is_positive(10))
+
+
+
+def is_positive(number):
+    
+    intiger = number
+
+    if (intiger %2 ==0 ) and (intiger % 3 ==0 ):
+        return(intiger, "is true or fulse")
+    if intiger %2 ==0:
+        return intiger, "is true"  
+    if intiger % 3 == 0:
+        return intiger, "is fulse"   
+    
+print(is_positive(15))
+
+
+
+def is_positive(number):
+    
+    intiger = number
+
+    if (intiger %2 ==0 ) and (intiger % 3 ==0 ):
+        return(intiger, "is true or fulse")
+    if intiger %2 ==0:
+        return intiger, "is true"  
+    if intiger % 3 == 0:
+        return intiger, "is fulse"   
+    
+print(is_positive(18))

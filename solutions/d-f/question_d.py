a5a2a6323f2d2180a430f3ddd353e279149f727a
akorede_mujaheed <akorede_mujaheed>
2025-07-04 15:58:49 +0100

    reverse_string solved

---
+++ solutions/d-f/question_d.py
@@ -0,0 +1,26 @@
+def count_vowel(text):
+    vowels = "aeiou"
+    count = 0
+    for char in text . lower():
+        if char in vowels:
+            count += 1
+
+    return count 
+
+
+print(count_vowel("hello"))
+
+# EXAMPLE 2
+
+def count_vowel(text):
+    vowels = "aeiou"
+    count = 0
+    for char in text . lower():
+        if char in vowels:
+            count += 1
+
+    return count 
+
+
+print(count_vowel("python"))
+
---
+++ solutions/d-f/question_e.py
@@ -0,0 +1,6 @@
+def reverse_string(s):
+    s= reversed("i searched on google symboles that returns the reverse of a string ")
+    print(list(s))
+    
+reverse_string("i searched on google symboles that returns the reverse of a string")
+
